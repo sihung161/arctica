ff40adfc8df1978fa9afa938cce084d57b8bbe23
Sĩ Hùng <hungnguyensi161@gmail.com>
2022-05-16 21:26:21 +0700

    first commit

---
+++ js/function.js
@@ -0,0 +1,21 @@
+﻿$(function(){
+
+    new WOW().init();
+    
+    var $khoimonan = $('.nhieumon').isotope({
+        itemSelector: '.motmon',
+        layoutMode: 'masonry'
+    });
+
+    // layout Isotope after each image loads
+    $khoimonan.imagesLoaded().progress( function() {
+        $khoimonan.isotope('layout');
+    });
+
+    $('.tieudect a').click(function() {
+        dulieu = $(this).data('monan');
+        $khoimonan.isotope({ filter: dulieu });
+        return false;
+    });
+})  
+ 
